0dfeb871f05a2d618cf9bbbd53c9b7c11c4c56a3
Ashlee Tan <ash@ashlee.one>
2022-08-21 19:39:15 +0800

    fixed issue

---
+++ src/main/java/com/meowbie/Main.java
@@ -0,0 +1,27 @@
+package com.meowbie;
+
+import com.meowbie.commands.CalculateCommand;
+import com.meowbie.events.HelloEvent;
+import com.meowbie.commands.PingCommand;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+    public static void main(String[] args) throws LoginException {
+        String discordToken = System.getenv("DISCORD_TOKEN");
+        String nowPlaying = "Stardew Valley";
+
+        JDA jda = JDABuilder.createDefault(discordToken)
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                .setActivity(Activity.playing(nowPlaying))
+                .build();
+
+        jda.addEventListener(new HelloEvent());
+        jda.addEventListener(new PingCommand());
+        jda.addEventListener(new CalculateCommand());
+    }
+}
